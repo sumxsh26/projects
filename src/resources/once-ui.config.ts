3451aed19b364fc2293076cfdf3446c4bc813c7f
Sumayyah Zaini <smyyhzaii@gmail.com>
2025-10-29 15:39:55 +0800

    testing push into projects

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import type {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com";
+const baseURL: string = "https://sumxsh26.github.io/projects";
 
 const routes: RoutesConfig = {
   "/": true,
