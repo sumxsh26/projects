d19cb46186a129cf60155e1bd69572baac4d3fe3
sumxsh26 <smyyhzaii@gmail.com>
2025-11-01 17:45:55 +0800

    header change

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -49,7 +49,7 @@ const home: Home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>developer by day, designer by heart</>,
+  headline: <>junior web developer and designer</>,
   featured: {
     display: true,
     title: (
